11f03c37888f6880b89cf4576174878536f364ee
Hizashii <99464728+Hizashii@users.noreply.github.com>
2025-05-24 00:39:19 +0200

    frontend generate

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -3,6 +3,7 @@ import { resolve } from 'path'
 export default defineNuxtConfig({
   compatibilityDate: '2025-04-03',
   devtools: { enabled: true },
+  ssr: false,
 
   modules: ['@nuxtjs/i18n', '@nuxtjs/tailwindcss'],
 
@@ -18,12 +19,17 @@ export default defineNuxtConfig({
       { code: 'en-GB', iso: 'en-GB', file: 'en.json' }
     ]
   },
+
   nitro: {
     devProxy: {
       '/api/**':      'http://localhost:5000',
       '/uploads/**':  'http://localhost:5000'
+    },
+    prerender: {
+      failOnError: false
     }
   },
+
   runtimeConfig: {
     public: {
       apiBase: process.env.NUXT_PUBLIC_API_BASE || ''
